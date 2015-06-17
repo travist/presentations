79c0d20b0c77f99037ce408e069792af6f6b0615
Travis Tidwell <codertravist@gmail.com>
2015-06-17 17:01:40 -0500

    Updating the presentation.

--- javascript-essentials/presentation.js
+++ javascript-essentials/presentation.js
@@ -28,12 +28,14 @@
 
 
 
+
 /**
  * Patterns are just as important as Syntax.
  *
  *  - http://bit.ly/1de5qfT
  *  - http://bit.ly/1atTe8t
  */
+
 
 
 
@@ -88,18 +90,20 @@
 
 
 
-
 /**
  * Functions in JavaScript
  *
  *  - Functions are "first-class" objects.
  *  - Functions can be anonymous.
  *  - Functions can be "self-executing"
- *  - Functions encapsulate and capture context.
+ *  - Functions encapsulate and capture scope.
  *  - Closures
- *  - Module pattern
- *  - Common.JS modules
+ *  - Module Pattern
+ *  - Common.JS Module Pattern
  */
+
+
+
 
 
 
@@ -133,13 +137,12 @@
  *  - http://bit.ly/VHXAYB
  *  - http://bit.ly/1SourKC
  *
- *  - Difference between context and scope.
+ *  - Difference between scope and context.
  *  - 'new' objects create context
  *  - 'this' is assigned to the object instance that 'invokes' a method.
  *  - You can bind context with 'bind'
  *  - You can call a method with context using 'call' or 'apply'
  */
-
 
 
 
@@ -173,8 +176,6 @@
  *  - You can create jQuery plugins using jQuery.fn and point to your class.
  *  - "Module Classes" combine Module pattern with OOP pattern.
  */
-
-
 
 
 
